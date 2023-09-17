0dd5819a64c1bfb7734a7a16a8aab165a4ce925f
Jonathan <77663900+Jhone6523@users.noreply.github.com>
2023-09-17 11:55:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -150,21 +150,6 @@ def upload_and_publish(data: UploadRequest, request: Request):
         text = data.caption
         text_color = (0, 0, 0)  # Couleur du texte (R, G, B)
 
-
-
-        if len(data.caption) < 30:
-            font_size = 92
-        elif 30 < len(data.caption) < 50:
-            font_size = 82
-        elif 50 < len(data.caption) < 75:
-            font_size = 72
-        elif 75 < len(data.caption) < 100:
-            font_size = 62
-        elif 100 < len(data.caption) < 150:
-            font_size = 52
-        else:
-            font_size = 32
-
         font = ImageFont.truetype("arial.ttf", font_size)  # Utilisez la police Arial avec une taille initiale
 
        # Calculez la largeur du texte avec la police actuelle
