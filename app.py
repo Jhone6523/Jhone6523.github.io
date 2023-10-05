f4fd76ee599fe7909c86d593e8776e7fda088b81
Jonathan <77663900+Jhone6523@users.noreply.github.com>
2023-10-05 08:21:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi import Request
 import json
 import datetime
+from PIL import ImageColor
 
 app = FastAPI()
 if __name__ == "__main__":
@@ -71,37 +72,32 @@ def upload_and_publish(data: UploadRequest, request: Request):
         
         image_cropee = image.crop((left, top, right, bottom))
         
-        image = image_cropee.resize((800, 800),Image.LANCZOS)
+        image = image_cropee.resize((1080, 1080),Image.LANCZOS)
         draw = ImageDraw.Draw(image)
 
         largeur, hauteur = image.size
 
 
-        point_depart = (0, hauteur // 1.5)  
+        point_depart = (0, hauteur // 1.56)  
         point_fin = (largeur, hauteur)  
 
 
         couleur_bandeau = (255, 255, 255)  
         draw.rectangle([point_depart, point_fin], fill=couleur_bandeau)
 
-        
-        couleur_contours = (197,68,62)  
-        largeur_bordure = 40
+        hex_color = "#e32527"
+
+        couleur_contours = ImageColor.getrgb(hex_color)
+        largeur_bordure = 100
         position_ligne = (point_depart[0], point_fin[1])
         longueur_ligne = largeur 
         draw.line([position_ligne, (position_ligne[0] + longueur_ligne, position_ligne[1])], fill=couleur_contours, width=largeur_bordure)
 
 
-        font_mentions = ImageFont.truetype("ArchivoNarrow-Bold.ttf", 20)
-        picture = "Photo: " + data.auteur
+        font_mentions = ImageFont.truetype("ArchivoNarrow-VariableFont_wght.ttf", 18)
         via = "via: " + data.via
-        if(data.auteur != ''):
-            mentions_text = picture + " \n " + via 
-        else : 
-            mentions_text = via
-        mentions_text_color = (255, 255, 255)  
-
-
+        mentions_text = via
+        mentions_text_color = (255, 255, 255)
        
         textbbox = draw.textbbox((0, 0), mentions_text, font=font_mentions)
 
@@ -109,16 +105,30 @@ def upload_and_publish(data: UploadRequest, request: Request):
 
 
         max_x = image.width - text_width - 10  
-        max_y = 10  
+        max_y = 2  
 
         x = min(max_x, max(10, max_x)) 
 
-        draw.text((x, max_y), mentions_text, fill=mentions_text_color, font=font_mentions)
+        draw.text((x, max_y), mentions_text, fill=mentions_text_color, font=font_mentions,resample=Image.LANCZOS)
+
+        picture = "Photo: " + data.auteur
+        mentions_text = picture
+        mentions_text_color = (255, 255, 255)
+       
+        textbbox = draw.textbbox((0, 0), mentions_text, font=font_mentions)
+
+        text_width = textbbox[2] - textbbox[0]
 
 
+        max_x = image.width - text_width - 10  
+        max_y = 22  
 
-        font_size = 42
-        
+        x = min(max_x, max(10, max_x)) 
+
+        draw.text((x, max_y), mentions_text, fill=mentions_text_color, font=font_mentions, resample=Image.LANCZOS)
+
+        font_size = 68
+        """
         if len(data.headline) < 30:
             font_size = 92
         elif 30 < len(data.headline) < 50:
@@ -133,7 +143,7 @@ def upload_and_publish(data: UploadRequest, request: Request):
             font_size = 32
         else:
             font_size = 32
-            
+        """
         text = data.headline
         text_color = (0, 0, 0)
 
@@ -171,9 +181,9 @@ def upload_and_publish(data: UploadRequest, request: Request):
 
             return centered_lines
 
-        wrapped_and_centered_lines = wrap_and_center_text(text, font, largeur - 35)
-        line_spacing = 10
-        current_y = point_depart[1] + 30
+        wrapped_and_centered_lines = wrap_and_center_text(text, font, largeur - 40)
+        line_spacing = 40
+        current_y = point_depart[1] + 40
         for line, centered_x in wrapped_and_centered_lines:
             textbbox = draw.textbbox((0, 0), line, font=font)
             text_height = textbbox[3] - textbbox[1]
@@ -188,22 +198,23 @@ def upload_and_publish(data: UploadRequest, request: Request):
         image_width, image_height = image.size
 
         # Définissez la taille et la position du rectangle au milieu de l'image
-        rect_width = 150
-        rect_height = 50
+        rect_width = 148
+        rect_height = 60
         rect_x = (image_width - rect_width) // 2  # Au milieu horizontalement
-        rect_y = (image_height - rect_height) // 1.48  # Un quart de la hauteur de l'image depuis le haut
+        rect_y = (image_height - rect_height) // 1.54  # Un quart de la hauteur de l'image depuis le haut
+
+        hex_color = "#e32527"
 
         # Définissez la couleur du rectangle
-        rect_color = (197,68,62)  # Rouge
+        rect_color = ImageColor.getrgb(hex_color)
 
         # Dessinez le rectangle
         draw.rectangle([rect_x, rect_y, rect_x + rect_width, rect_y + rect_height], fill=rect_color)
 
         # Définissez la couleur du texte et sa police
         text_color = (255, 255, 255)  # Blanc
-        date_font = ImageFont.truetype("ArchivoNarrow-Bold.ttf", 46)  # Choisissez la taille de police souhaitée
+        date_font = ImageFont.truetype("ArchivoNarrow-Bold.ttf", 56)  # Choisissez la taille de police souhaitée
 
-        # Obtenez la date actuelle
         now = "report"
 
         # Obtenez les dimensions du texte
@@ -211,12 +222,11 @@ def upload_and_publish(data: UploadRequest, request: Request):
         text_width = textbbox[2] - textbbox[0]
         text_height = textbbox[3] - textbbox[1]
 
-        # Calculez la position du texte au centre du rectangle
-
+        draw_text_psd_style(draw,(482, 650),now,date_font,tracking=-50,leading=32,fill="White")
 
         # Dessinez la date au centre du rectangle
-        draw.text((345, 494), now, fill=text_color, font=date_font)
-
+        #draw.text((480, 650), now, fill=text_color, font=date_font)
+    
 
         filename = str(uuid.uuid4()) + ".jpg"
         unique_filename = 'download-image/' + filename
@@ -225,12 +235,12 @@ def upload_and_publish(data: UploadRequest, request: Request):
         logo = Image.open("logo.png")
 
         # Redimensionnez le logo selon vos besoins
-        logo.thumbnail((100, 100))  # Ajustez la taille du logo selon vos préférences
+        logo.thumbnail((180, 110))  # Ajustez la taille du logo selon vos préférences
 
         # Superposez le logo en haut à gauche de l'image principale
         image.paste(logo, (10, 10), logo)
 
-        image.save(unique_filename)
+        image.save(unique_filename, "JPEG", quality=95)
 
         json_file_path = 'bdd_image.json'
 
@@ -273,3 +283,41 @@ async def afficher_fichier_html(request: Request):
 
     contenu_json.sort(key=lambda x: x['date'], reverse=True)
     return templates.TemplateResponse("template.html", {"request": request, "donnees": contenu_json})
+
+def draw_text_psd_style(draw, xy, text, font, tracking=0, leading=None, **kwargs):
+    """
+    usage: draw_text_psd_style(draw, (0, 0), "Test", 
+                tracking=-0.1, leading=32, fill="Blue")
+
+    Leading is measured from the baseline of one line of text to the
+    baseline of the line above it. Baseline is the invisible line on which most
+    letters—that is, those without descenders—sit. The default auto-leading
+    option sets the leading at 120% of the type size (for example, 12‑point
+    leading for 10‑point type).
+
+    Tracking is measured in 1/1000 em, a unit of measure that is relative to 
+    the current type size. In a 6 point font, 1 em equals 6 points; 
+    in a 10 point font, 1 em equals 10 points. Tracking
+    is strictly proportional to the current type size.
+    """
+    def stutter_chunk(lst, size, overlap=0, default=None):
+        for i in range(0, len(lst), size - overlap):
+            r = list(lst[i:i + size])
+            while len(r) < size:
+                r.append(default)
+            yield r
+    x, y = xy
+    font_size = font.size
+    lines = text.splitlines()
+    if leading is None:
+        leading = font.size * 1.2
+    for line in lines:
+        for a, b in stutter_chunk(line, 2, 1, ' '):
+            w = font.getlength(a + b) - font.getlength(b)
+            # dprint("[debug] kwargs")
+            print("[debug] kwargs:{}".format(kwargs))
+                
+            draw.text((x, y), a, font=font, **kwargs)
+            x += w + (tracking / 1000) * font_size
+        y += leading
+        x = xy[0]
